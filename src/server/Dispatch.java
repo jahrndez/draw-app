94a8a96238f5004aa3f387c6dcbb6bbaafbd27aa
Alex Hernandez <jahrndez@uw.edu>
2015-11-21 15:40:16 -0800

    removed uiDesigner.xml

--- src/server/Dispatch.java
+++ src/server/Dispatch.java
@@ -1,8 +1,13 @@
 package server;
 
+import java.net.Socket;
+
 /**
  * Will handle new connections and dispatch into individual sessions (games)
  */
 public class Dispatch {
+    Runnable r = () -> {
+        Socket listeningSocket = new Socket();
+    };
 
 }
