3e7f7302cd6607a3bb842c019fec5c627e79d489
Alex Hernandez <jahrndez@uw.edu>
2015-12-03 11:34:07 -0800

    TurnStartAlert handles drawer and guessers

--- src/interfaces/TurnStartAlert.java
+++ src/interfaces/TurnStartAlert.java
@@ -7,10 +7,28 @@ import java.io.Serializable;
  */
 public class TurnStartAlert extends LobbyMessage implements Serializable {
     private String drawerUsername;
+    private String word;
+    private boolean isDrawer;
 
     public TurnStartAlert(String drawerUsername) {
         this.type = MessageType.TURN_START;
         this.drawerUsername = drawerUsername;
+        this.isDrawer = false;
+    }
+
+    public TurnStartAlert(String drawerUsername, String word) {
+        this.type = MessageType.TURN_START;
+        this.drawerUsername = drawerUsername;
+        this.word = word;
+        this.isDrawer = true;
+    }
+
+    public boolean isDrawer() {
+        return isDrawer;
+    }
+
+    public String getWord() {
+        return word;
     }
 
     public String getDrawerUsername() {
